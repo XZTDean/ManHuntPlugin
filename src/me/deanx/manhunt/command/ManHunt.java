6190345f123231d54c8facd0225bd556105c2cc9
xztdean <xztdean@gmail.com>
2021-05-07 22:49:45 +0800

    Reset player's state in the beginning

--- src/me/deanx/manhunt/command/ManHunt.java
+++ src/me/deanx/manhunt/command/ManHunt.java
@@ -2,7 +2,11 @@ package me.deanx.manhunt.command;
 
 import me.deanx.manhunt.ManHuntPlugin;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.advancement.Advancement;
+import org.bukkit.advancement.AdvancementProgress;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,6 +14,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import java.util.Iterator;
 
 public class ManHunt implements CommandExecutor {
     public ManHunt(ManHuntPlugin plugin) {
@@ -59,18 +65,35 @@ public class ManHunt implements CommandExecutor {
         runner.getInventory().setBoots(new ItemStack(Material.LEATHER_BOOTS));
         runner.getInventory().addItem(new ItemStack(Material.WOODEN_AXE));
         runner.getInventory().addItem(new ItemStack(Material.OAK_LOG, 5));
+        setInitialState(runner);
         runner.sendMessage("You are Runner. RUN!");
     }
 
     private void setHunter(Player hunter) {
         hunter.getInventory().clear();
         hunter.getInventory().addItem(new ItemStack(Material.COMPASS));
-        runner.sendMessage("You are hunter, please wait for 15s.");
         hunter.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 300, 0));
         hunter.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 300, 128));
         hunter.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 300, 128));
         hunter.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 300, 128));
         hunter.setBedSpawnLocation(runner.getLocation());
+        setInitialState(hunter);
+        hunter.sendMessage("You are hunter, please wait for 15s.");
+    }
+
+    private void setInitialState(Player player) {
+        double maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
+        player.setHealth(maxHealth);
+        player.setFoodLevel(20);
+        player.setSaturation(5);
+        player.setGameMode(GameMode.SURVIVAL);
+        // reset advancement
+        for(Iterator<Advancement> iterator = Bukkit.getServer().advancementIterator(); iterator.hasNext();) {
+            AdvancementProgress progress = player.getAdvancementProgress(iterator.next());
+            for (String criteria : progress.getAwardedCriteria())
+                progress.revokeCriteria(criteria);
+        }
+
     }
 
     private String labelPlayer(String name) {
